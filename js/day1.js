750388970e2a78fc998c2a79a0caa23122bc0157
Pavel Djundik <github@xpaw.me>
2015-12-03 19:11:13 +0200

    Reduce copypaste code by generating forms

--- js/day1.js
+++ js/day1.js
@@ -1,54 +1,36 @@
-(function()
+window.AdventOfCode.Day1 = function( input )
 {
-	window.AdventOfCode.Day1 = function( input )
-	{
-		var floor = 0;
-		var position = -1;
-		
-		// Santa starts on the ground floor (floor 0) and then
-		// follows the instructions one character at a time.
-		for( var i = 0; i < input.length; i++ )
-		{
-			// An opening parenthesis, (, means he should go up one floor.
-			if( input[ i ] === '(' )
-			{
-				floor++;
-			}
-			
-			// A closing parenthesis, ), means he should go down one floor.
-			if( input[ i ] === ')' )
-			{
-				floor--;
-			}
-			
-			// Now, given the same instructions, find the position of the first
-			// character that causes him to enter the basement (floor -1).
-			// The first character in the instructions has position 1,
-			// the second character has position 2, and so on.
-			if( floor === -1 && position < 0 )
-			{
-				// ) causes him to enter the basement at character position 1.
-				// ()()) causes him to enter the basement at character position 5.
-				// This is 1-indexed.
-				position = i + 1;
-			}
-		}
-		
-		return [ floor, position ];
-	};
+	var floor = 0;
+	var position = -1;
 	
-	document.getElementById( 'day1-textarea' ).addEventListener( 'change', function()
+	// Santa starts on the ground floor (floor 0) and then
+	// follows the instructions one character at a time.
+	for( var i = 0; i < input.length; i++ )
 	{
-		var input = this.value.trim();
-		
-		if( input.length === 0 )
+		// An opening parenthesis, (, means he should go up one floor.
+		if( input[ i ] === '(' )
 		{
-			return;
+			floor++;
 		}
 		
-		var solution = window.AdventOfCode.Day1( input );
+		// A closing parenthesis, ), means he should go down one floor.
+		if( input[ i ] === ')' )
+		{
+			floor--;
+		}
 		
-		document.getElementById( 'day1-partone' ).textContent = solution[ 0 ];
-		document.getElementById( 'day1-parttwo' ).textContent = solution[ 1 ];
-	}, false );
-}());
+		// Now, given the same instructions, find the position of the first
+		// character that causes him to enter the basement (floor -1).
+		// The first character in the instructions has position 1,
+		// the second character has position 2, and so on.
+		if( floor === -1 && position < 0 )
+		{
+			// ) causes him to enter the basement at character position 1.
+			// ()()) causes him to enter the basement at character position 5.
+			// This is 1-indexed.
+			position = i + 1;
+		}
+	}
+	
+	return [ floor, position ];
+};
--- js/day2.js
+++ js/day2.js
@@ -1,72 +1,54 @@
-(function()
+window.AdventOfCode.Day2 = function( input )
 {
-	window.AdventOfCode.Day2 = function( input )
-	{
-		input = input.split( '\n' );
-		
-		var completeArea = 0;
-		var completeRibbon = 0;
-		
-		for( var i = 0; i < input.length; i++ )
-		{
-			// They have a list of the dimensions (length l, width w, and height h)
-			// of each present, and only want to order exactly as much as they need.
-			var present = input[ i ]
-				.split( 'x' )
-				.sort( function( a, b )
-				{
-					return a - b;
-				} );
-			
-			var sides =
-			[
-				present[ 0 ] * present[ 1 ], // l*w
-				present[ 1 ] * present[ 2 ], // w*h
-				present[ 2 ] * present[ 0 ]  // h*l
-			]
-			
-			// Find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l.
-			var area =
-				2 * sides[ 0 ] +
-				2 * sides[ 1 ] +
-				2 * sides[ 2 ];
-			
-			// The elves also need a little extra paper for each present:
-			// the area of the smallest side.
-			completeArea += area + sides[ 0 ];
-			
-			// The ribbon required to wrap a present is the shortest distance
-			// around its sides, or the smallest perimeter of any one face.
-			// Each present also requires a bow made out of ribbon as well;
-			// the feet of ribbon required for the perfect bow is equal to
-			// the cubic feet of volume of the present.
-			var ribbon =
-				2 * present[ 0 ] +
-				2 * present[ 1 ];
-			
-			var bow =
-				present[ 0 ] *
-				present[ 1 ] *
-				present[ 2 ];
-			
-			completeRibbon += ribbon + bow;
-		}
-		
-		return [ completeArea, completeRibbon ];
-	};
+	input = input.split( '\n' );
 	
-	document.getElementById( 'day2-textarea' ).addEventListener( 'change', function()
+	var completeArea = 0;
+	var completeRibbon = 0;
+	
+	for( var i = 0; i < input.length; i++ )
 	{
-		var input = this.value.trim();
+		// They have a list of the dimensions (length l, width w, and height h)
+		// of each present, and only want to order exactly as much as they need.
+		var present = input[ i ]
+			.split( 'x' )
+			.sort( function( a, b )
+			{
+				return a - b;
+			} );
 		
-		if( input.length === 0 )
-		{
-			return;
-		}
+		var sides =
+		[
+			present[ 0 ] * present[ 1 ], // l*w
+			present[ 1 ] * present[ 2 ], // w*h
+			present[ 2 ] * present[ 0 ]  // h*l
+		]
 		
-		var solution = window.AdventOfCode.Day2( input );
+		// Find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l.
+		var area =
+			2 * sides[ 0 ] +
+			2 * sides[ 1 ] +
+			2 * sides[ 2 ];
 		
-		document.getElementById( 'day2-partone' ).textContent = solution[ 0 ];
-		document.getElementById( 'day2-parttwo' ).textContent = solution[ 1 ];
-	}, false );
-}());
+		// The elves also need a little extra paper for each present:
+		// the area of the smallest side.
+		completeArea += area + sides[ 0 ];
+		
+		// The ribbon required to wrap a present is the shortest distance
+		// around its sides, or the smallest perimeter of any one face.
+		// Each present also requires a bow made out of ribbon as well;
+		// the feet of ribbon required for the perfect bow is equal to
+		// the cubic feet of volume of the present.
+		var ribbon =
+			2 * present[ 0 ] +
+			2 * present[ 1 ];
+		
+		var bow =
+			present[ 0 ] *
+			present[ 1 ] *
+			present[ 2 ];
+		
+		completeRibbon += ribbon + bow;
+	}
+	
+	return [ completeArea, completeRibbon ];
+};
