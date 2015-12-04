e83dac28117f332e37661cf47b5d58d587de2bd3
Pavel Djundik <xPaw@users.noreply.github.com>
2015-12-04 18:36:40 +0200

    Day 4 golfed

--- codegolf.js
+++ codegolf.js
@@ -8,3 +8,6 @@ function Day2(b){b=b.split("\n");for(var d=0,e=0,f=0;f<b.length;f++)var a=b[f].s
 
 // Day 3 - 216 characters
 function Day3(g){function b(b){for(var h=1,k={"0x0":1},d=0;d<b;d++)for(var e=0,f=0,c=d;c<g.length;c+=b){var a=g[c];"^"==a?e++:"v"==a?e--:">"==a?f++:"<"==a&&f--;a=e+"x"+f;k[a]||(h++,k[a]=1)}return h}return[b(1),b(2)]}
+
+// Day 4 - 136 characters
+function Day4(c){function a(a){for(var b=0,d="0".repeat(a);SparkMD5.hashBinary(c+ ++b).substring(0,a)!==d;);return b}return[a(5),a(6)]};
