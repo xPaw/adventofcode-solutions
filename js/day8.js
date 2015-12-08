e1e5ca1e8df64bed8177ca6fc555a4222bb0e964
Pavel Djundik <github@xpaw.me>
2015-12-08 12:33:30 +0200

    Day 8 solution

---
+++ js/day8.js
@@ -0,0 +1,48 @@
+window.AdventOfCode.Day8 = function( input )
+{
+	var input = input.split( '\n' );
+	
+	var totalLength = 0;
+	var cleanLength = 0;
+	var encodedLength = 0;
+	
+	for( var i = 0; i < input.length; i++ )
+	{
+		var str = input[ i ];
+		var escaping = false;
+		
+		totalLength += str.length;
+		encodedLength += str.length + 4;
+		
+		for( var x = 1; x < str.length - 1; x++ )
+		{
+			var char = str[ x ];
+			
+			if( escaping )
+			{
+				escaping = false;
+				
+				if( char === 'x' )
+				{
+					x += 2;
+				}
+				else
+				{
+					encodedLength++;
+				}
+			}
+			else if( char === '\\' )
+			{
+				encodedLength++;
+				
+				escaping = true;
+				
+				continue;
+			}
+			
+			cleanLength++;
+		}
+	}
+	
+	return [ totalLength - cleanLength, encodedLength - totalLength ];
+};
