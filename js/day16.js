950aa1b19698e21961f723c8a60e28812bcc8ef9
Pavel Djundik <github@xpaw.me>
2015-12-16 12:14:20 +0200

    Day 16 solution

---
+++ js/day16.js
@@ -0,0 +1,54 @@
+window.AdventOfCode.Day16 = function( input )
+{
+	var thingsYouRemember =
+	{
+		children   : '3',
+		cats       : '7',
+		samoyeds   : '2',
+		pomeranians: '3',
+		akitas     : '0',
+		vizslas    : '0',
+		goldfish   : '5',
+		trees      : '3',
+		cars       : '2',
+		perfumes   : '1'
+	};
+	
+	input = input.replace( /[,:]/g, '' ).split( '\n' ).map( function( aunt )
+	{
+		return aunt.split( ' ' );
+	} );
+	
+	var dayOne = input.filter( function( aunt )
+	{
+		return (
+			thingsYouRemember[ aunt[ 2 ] ] == aunt[ 3 ] &&
+			thingsYouRemember[ aunt[ 4 ] ] == aunt[ 5 ] &&
+			thingsYouRemember[ aunt[ 6 ] ] == aunt[ 7 ] );
+	} );
+	
+	var filterPartTwo = function( what, count )
+	{
+		if( what === 'cats' || what === 'trees' )
+		{
+			return thingsYouRemember[ what ] < count;
+		}
+		
+		if( what === 'pomeranians' || what === 'goldfish' )
+		{
+			return thingsYouRemember[ what ] > count;
+		}
+		
+		return thingsYouRemember[ what ] == count;
+	};
+	
+	var dayTwo = input.filter( function( aunt )
+	{
+		return (
+			filterPartTwo( aunt[ 2 ], aunt[ 3 ] ) &&
+			filterPartTwo( aunt[ 4 ], aunt[ 5 ] ) &&
+			filterPartTwo( aunt[ 6 ], aunt[ 7 ] ) );
+	} );
+	
+	return [ dayOne[ 0 ][ 1 ], dayTwo[ 0 ][ 1 ] ];
+};
