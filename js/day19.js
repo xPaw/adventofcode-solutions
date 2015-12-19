b4b13914ab3069dabd5d196f0e10f1666d0af783
Pavel Djundik <github@xpaw.me>
2015-12-19 16:38:43 +0200

    Solve day 19 part 2

--- js/day19.js
+++ js/day19.js
@@ -11,10 +11,12 @@ window.AdventOfCode.Day19 = function( input )
 	} );
 	
 	var uniqueNewMolecules = {};
+	var steps = 0;
+	var i, y;
 	
-	for( var i = 0; i < molecule.length; i++ )
+	for( i = 0; i < molecule.length; i++ )
 	{
-		for( var y = 0; y < input.length; y++ )
+		for( y = 0; y < input.length; y++ )
 		{
 			if( molecule.substr( i, input[ y ][ 0 ].length ) === input[ y ][ 0 ] )
 			{
@@ -23,5 +25,51 @@ window.AdventOfCode.Day19 = function( input )
 		}
 	}
 	
-	return [ Object.keys( uniqueNewMolecules ).length, 0 ];
+	// http://stackoverflow.com/a/6274398/2200891
+	function shuffle(array)
+	{
+		var counter = array.length, temp, index;
+		
+		while (counter > 0)
+		{
+			index = Math.floor(Math.random() * counter);
+			
+			counter--;
+			
+			temp = array[counter];
+			array[counter] = array[index];
+			array[index] = temp;
+		}
+		
+		return array;
+	}
+	
+	var target = molecule;
+	
+	while( target !== 'e' )
+	{
+		var lastTarget = target;
+		
+		for( y = 0; y < input.length; y++ )
+		{
+			i = target.indexOf( input[ y ][ 1 ] );
+			
+			if( i > -1 )
+			{
+				steps++;
+				
+				target = target.slice( 0, i ) + input[ y ][ 0 ] + target.slice( i + input[ y ][ 1 ].length );
+			}
+		}
+		
+		if( lastTarget === target )
+		{
+			// Shuffling is hilariously good enough to solve this
+			input = shuffle( input );
+			target = molecule;
+			steps = 0;
+		}
+	}
+	
+	return [ Object.keys( uniqueNewMolecules ).length, steps ];
 };
