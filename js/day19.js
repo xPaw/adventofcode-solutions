0a77f7af86eefa8955e8ebc8e71b22fadff4641d
Pavel Djundik <github@xpaw.me>
2015-12-19 22:25:22 +0200

    askalski's approach to day 19 part 2

--- js/day19.js
+++ js/day19.js
@@ -1,75 +1,58 @@
 window.AdventOfCode.Day19 = function( input )
 {
-	input = input.split( '\n' );
+	input = input.split( '' ).reverse().join( '' ).split( '\n' );
 	
-	var molecule = input.pop();
-	input.pop(); // empty line
+	var molecule = input.shift();
+	input.shift(); // empty line
 	
 	input = input.map( function( a )
 	{
-		return a.split( ' => ' );
+		return a.split( ' >= ' );
 	} );
 	
 	var uniqueNewMolecules = {};
-	var steps = 0;
 	var i, y;
 	
 	for( i = 0; i < molecule.length; i++ )
 	{
 		for( y = 0; y < input.length; y++ )
 		{
-			if( molecule.substr( i, input[ y ][ 0 ].length ) === input[ y ][ 0 ] )
+			if( molecule.substr( i, input[ y ][ 1 ].length ) === input[ y ][ 1 ] )
 			{
-				uniqueNewMolecules[ molecule.slice( 0, i ) + input[ y ][ 1 ] + molecule.slice( i + input[ y ][ 0 ].length ) ] = true;
+				uniqueNewMolecules[ molecule.slice( 0, i ) + input[ y ][ 0 ] + molecule.slice( i + input[ y ][ 1 ].length ) ] = true;
 			}
 		}
 	}
 	
-	// http://stackoverflow.com/a/6274398/2200891
-	function shuffle(array)
-	{
-		var counter = array.length, temp, index;
-		
-		while (counter > 0)
-		{
-			index = Math.floor(Math.random() * counter);
-			
-			counter--;
-			
-			temp = array[counter];
-			array[counter] = array[index];
-			array[index] = temp;
-		}
-		
-		return array;
-	}
+	var steps = 0;
+	var lookup = {};
+	var replaced = false;
 	
-	var target = molecule;
-	
-	while( target !== 'e' )
+	input = input.map( function( a )
 	{
-		var lastTarget = target;
+		lookup[ a[ 0 ] ] = a[ 1 ];
 		
-		for( y = 0; y < input.length; y++ )
-		{
-			i = target.indexOf( input[ y ][ 1 ] );
-			
-			if( i > -1 )
-			{
-				steps++;
-				
-				target = target.slice( 0, i ) + input[ y ][ 0 ] + target.slice( i + input[ y ][ 1 ].length );
-			}
-		}
+		return a[ 0 ];
+	} );
+	
+	molecule = molecule;
+	
+	var regexp = new RegExp( '(' + input.join( '|' ) + ')', 'g' );
+	var replaceCallback = function( matched )
+	{
+		replaced = true;
 		
-		if( lastTarget === target )
-		{
-			// Shuffling is hilariously good enough to solve this
-			input = shuffle( input );
-			target = molecule;
-			steps = 0;
-		}
+		steps++;
+		
+		return lookup[ matched ];
+	};
+	
+	do
+	{
+		replaced = false;
+		molecule = molecule.replace( regexp, replaceCallback );
 	}
+	while( replaced );
 	
 	return [ Object.keys( uniqueNewMolecules ).length, steps ];
 };
