cdead531168de3f2d0d238a2ee91bf3795c8db98
Pavel Djundik <github@xpaw.me>
2016-12-17 17:33:01 +0200

    2016 Day 5

---
+++ 2016/js/day5.js
@@ -0,0 +1,39 @@
+window.AdventOfCode.Day5 = function( input )
+{
+	let i = 2307654;
+	let hash;
+	let part1password = '';
+	let part2password = [];
+	
+	do
+	{
+		hash = SparkMD5.hash( input + i++ );
+		
+		if( hash[ 0 ] === '0'
+		&&  hash[ 1 ] === '0'
+		&&  hash[ 2 ] === '0'
+		&&  hash[ 3 ] === '0'
+		&&  hash[ 4 ] === '0' )
+		{
+			let position = hash[ 5 ];
+			
+			if( part1password.length < 8 )
+			{
+				part1password += position;
+			}
+			
+			if( position >= 0 && position <= 7 && part2password[ position ] === undefined )
+			{
+				part2password[ position ] = hash[ 6 ];
+			}
+			
+			if( part1password.length === 8 && part2password.filter( () => true ).length === 8 )
+			{
+				break;
+			}
+		}
+	}
+	while( true );
+	
+	return [ part1password, part2password.join( '' ) ];
+};
