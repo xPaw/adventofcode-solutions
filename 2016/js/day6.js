2dd1d1ad257e584bd1476af329ea9ec5906f75f0
Pavel Djundik <github@xpaw.me>
2016-12-17 23:02:22 +0200

    2016 Day 6

---
+++ 2016/js/day6.js
@@ -0,0 +1,50 @@
+window.AdventOfCode.Day6 = function( input )
+{
+	input = input.split( '\n' );
+	
+	let countedLetters = [];
+	
+	for( const line of input )
+	{
+		for( let i = 0; i < line.length; i++ )
+		{
+			if( !countedLetters[ i ] )
+			{
+				countedLetters[ i ] = new Map();
+			}
+			
+			countedLetters[ i ].set( line[ i ], 1 + countedLetters[ i ].get( line[ i ] ) || 0 );
+		}
+	}
+	
+	let message1 = "";
+	let message2 = "";
+	
+	for( const letters of countedLetters )
+	{
+		let commonLetter = '';
+		let commonLetterCount = 0;
+		let notCommonLetter = '';
+		let notCommonLetterCount = Infinity;
+		
+		for( const letter of letters )
+		{
+			if( commonLetterCount < letter[ 1 ] )
+			{
+				commonLetterCount = letter[ 1 ];
+				commonLetter = letter[ 0 ];
+			}
+			
+			if( notCommonLetterCount > letter[ 1 ] )
+			{
+				notCommonLetterCount = letter[ 1 ];
+				notCommonLetter = letter[ 0 ];
+			}
+		}
+		
+		message1 += commonLetter;
+		message2 += notCommonLetter;
+	}
+	
+	return [ message1, message2 ];
+};
