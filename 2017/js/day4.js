49b27d5b23b5a943a94351fd6b69f0ef3db70d86
Pavel Djundik <github@xpaw.me>
2017-12-04 11:34:19 +0200

    2017 Day 4

---
+++ 2017/js/day4.js
@@ -0,0 +1,22 @@
+window.AdventOfCode.Day4 = function( input )
+{
+	input = input.split( '\n' ).map( ( pass ) => pass.split( ' ' ) );
+	
+	const countValid = ( counter, pass ) =>
+	{
+		if( ( new Set( pass ) ).size === pass.length )
+		{
+			counter++;
+		}
+		
+		return counter;
+	};
+	
+	const part1 = input.reduce( countValid, 0 );
+	
+	const part2 = input
+		.map( ( pass ) => pass.map( ( word ) => [ ...word ].sort( ).join( '' ) ) )
+		.reduce( countValid, 0 );
+	
+	return [ part1, part2 ];
+};
