c2b06534b7a58d570f5248b7dfea993fda4662a5
Pavel Djundik <github@xpaw.me>
2017-12-08 15:11:28 +0200

    2017 Day 8

---
+++ 2017/js/day8.js
@@ -0,0 +1,61 @@
+window.AdventOfCode.Day8 = function( input )
+{
+	input = input.split( /\n/ );
+	
+	const Token =
+	{
+		Variable: 0,
+		Modifier: 1,
+		Value: 2,
+		ConditionVariable: 4,
+		Condition: 5,
+		ConditionValue: 6,
+	};
+	
+	const registers = {};
+	let part2 = 0;
+	
+	const IsConditionSatisfied = ( variable, condition, value ) =>
+	{
+		if( condition === '>'  ) return variable > value;
+		if( condition === '<'  ) return variable < value;
+		if( condition === '>=' ) return variable >= value;
+		if( condition === '<=' ) return variable <= value;
+		if( condition === '==' ) return variable == value;
+		if( condition === '!=' ) return variable != value;
+		
+	};
+	
+	input.forEach( ( line ) =>
+	{
+		line = line.split( ' ' );
+		
+		if( IsConditionSatisfied(
+			registers[ line[ Token.ConditionVariable ] ] || 0,
+			line[ Token.Condition ],
+			line[ Token.ConditionValue ]
+		) )
+		{
+			const variable = line[ Token.Variable ];
+			value = ( registers[ variable ] || 0 );
+			
+			if( line[ Token.Modifier ] === 'inc' )
+			{
+				value += +line[ Token.Value ];
+			}
+			else
+			{
+				value -= +line[ Token.Value ];
+			}
+			
+			if( part2 < value )
+			{
+				part2 = value;
+			}
+			
+			registers[ variable ] = value;
+		}
+	} );
+	
+	return [ Math.max( ...Object.values( registers ) ), part2 ];
+};
