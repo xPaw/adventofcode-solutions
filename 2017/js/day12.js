82b0723f75efc4c859da3898e70da95ba642e8d8
Pavel Djundik <github@xpaw.me>
2017-12-12 12:27:00 +0200

    2017 Day 12

---
+++ 2017/js/day12.js
@@ -0,0 +1,31 @@
+window.AdventOfCode.Day12 = function( input )
+{
+	input = input
+		.split( '\n' )
+		.map( ( line ) => line
+			.split( ' <-> ' )[ 1 ]
+			.split( ', ' )
+			.map( ( id ) => parseInt( id, 10 ) )
+		);
+	
+	const visited = new Map();
+	
+	const visit = ( value, id ) =>
+	{
+		if( visited.has( id ) )
+		{
+			return 0;
+		}
+		
+		visited.set( id, true );
+		
+		return value.reduce( ( total, nextId ) => total + visit( input[ nextId ], nextId ), 1 );
+	};
+	
+	input = input.map( visit );
+	
+	const part1 = input[ 0 ];
+	const part2 = input.reduce( ( total, x ) => x > 0 ? ++total : total, 0 );
+	
+	return [ part1, part2 ];
+};
