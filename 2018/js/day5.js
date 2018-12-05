a4104645a8dcc9993d7a0e85af7650f42186075f
Pavel Djundik <xPaw@users.noreply.github.com>
2018-12-05 11:03:22 +0200

    2018 Day 5

---
+++ 2018/js/day5.js
@@ -0,0 +1,36 @@
+window.AdventOfCode.Day5 = ( input ) =>
+{
+	const reactions = [];
+
+	for( let i = 65; i <= 90; i++ )
+	{
+		const uppercase = String.fromCodePoint( i );
+		const lowercase = String.fromCodePoint( i + 32 );
+
+		reactions.push( uppercase + lowercase );
+		reactions.push( lowercase + uppercase );
+	}
+
+	const reduceReactions = new RegExp( reactions.join( '|' ) );
+	const reducedLengths = [];
+
+	// 64 is @ which doesn't actually replace anything and we use that for part 1
+	for( let i = 64; i <= 90; i++ )
+	{
+		const uppercase = String.fromCodePoint( i );
+
+		let previous;
+		let current = input.replace( new RegExp( uppercase, 'ig' ), '' );
+
+		do
+		{
+			previous = current;
+			current = current.replace( reduceReactions, '' );
+		}
+		while( previous !== current );
+
+		reducedLengths.push( current.length );
+	}
+
+	return [ reducedLengths[ 0 ], Math.min( ...reducedLengths ) ];
+};
