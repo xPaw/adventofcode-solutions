cc7e58aa824eaabd3beed97e8e601b5a1a590bf1
Pavel Djundik <xPaw@users.noreply.github.com>
2018-12-07 21:16:29 +0200

    2018 Day 7

---
+++ 2018/js/day7.js
@@ -0,0 +1,58 @@
+window.AdventOfCode.Day7 = ( input ) =>
+{
+	input = input
+		.split( '\n' )
+		.map( l =>
+		{
+			l = l.split( ' ' );
+
+			return [ l[ 1 ], l[ 7 ] ];
+		} );
+
+	let graph = new Map();
+
+	for( const pair	of input )
+	{
+		if( !graph.has( pair[ 0 ] ) )
+		{
+			graph.set( pair[ 0 ], new Set() );
+		}
+
+		if( !graph.has( pair[ 1 ] ) )
+		{
+			graph.set( pair[ 1 ], new Set() );
+		}
+
+		graph.get( pair[ 0 ] ).add( pair[ 1 ] );
+	}
+
+	let part1 = '';
+	graph = new Map( [ ...graph.entries() ].sort() );
+	const graph2 = new Map( graph );
+
+	while( graph.size > 0 )
+	{
+		for( const set of graph )
+		{
+			let depends = false;
+
+			for( const set2 of graph )
+			{
+				if( set2[ 1 ].has( set[ 0 ] ) )
+				{
+					depends = true;
+					break;
+				}
+			}
+			
+			if( !depends )
+			{
+				part1 += set[ 0 ];
+				graph.delete( set[ 0 ] );
+				break;
+			}
+		}
+	}
+
+	return [ part1, 0 ];
+};
