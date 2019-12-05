8c725bbdb8a81d36e32312f25b98f039d8fd513e
Pavel Djundik <xPaw@users.noreply.github.com>
2019-12-05 15:54:42 +0200

    2019 Day 1

---
+++ 2019/js/day1.js
@@ -0,0 +1,18 @@
+window.AdventOfCode.Day1 = ( input ) =>
+{
+	input = input
+		.split( '\n' )
+		.map( x => Math.floor( x / 3 ) - 2 );
+
+	const part1 = input.reduce( ( x, y ) => x + y, 0 );
+	let part2 = part1;
+
+	do
+	{
+		input = input.map( x => Math.floor( x / 3 ) - 2 );
+		part2 += input.reduce( ( x, y ) => y > 0 ? x + y : x, 0 );
+	}
+	while( input.some( x => x > 0 ) );
+
+	return [ part1, part2 ];
+};
