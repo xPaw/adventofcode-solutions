b70c7991d1a640182b006e33228211814526ab19
Pavel Djundik <xPaw@users.noreply.github.com>
2019-12-06 14:07:47 +0200

    2019 Day 4

---
+++ 2019/js/day4.js
@@ -0,0 +1,60 @@
+module.exports = ( input ) =>
+{
+	input = input.split( '-' );
+	let start = +input[ 0 ];
+	const end = +input[ 1 ];
+	let part1 = 0;
+	let part2 = 0;
+	
+	do
+	{
+		const pass = start.toString();
+		part1 += checkPassword( pass );
+		part2 += checkPassword2( pass );
+	}
+	while( ++start < end );
+
+	return [ part1, part2 ];
+};
+
+function checkPassword( pass )
+{
+	let hasDouble = 0;
+
+	for( let i = 1; i < 6; i++ )
+	{
+		const a = pass[ i - 1 ];
+		const b = pass[ i ];
+
+		if( a > b )
+		{
+			return 0;
+		}
+
+		hasDouble |= a === b;
+	}
+
+	return hasDouble;
+}
+
+function checkPassword2( pass )
+{
+	let hasDouble = 0;
+	let prev = -1;
+
+	for( let i = 1; i < 6; i++ )
+	{
+		const a = pass[ i - 1 ];
+		const b = pass[ i ];
+
+		if( a > b )
+		{
+			return 0;
+		}
+
+		hasDouble |= a === b && b !== pass[i + 1] && prev !== a;
+		prev = a;
+	}
+
+	return hasDouble;
+}
