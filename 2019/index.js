7bd2fef4889862888d85f39d9b7e8b9c23ac4185
Pavel Djundik <xPaw@users.noreply.github.com>
2019-12-08 16:31:49 +0200

    Add benchmarking

--- 2019/index.js
+++ 2019/index.js
@@ -2,7 +2,7 @@
 
 process.chdir( __dirname );
 
-const fs = require( 'fs' );
+const { readFileSync } = require( 'fs' );
 const day = parseInt( process.argv[ 2 ], 10 );
 
 if( isNaN( day ) )
@@ -14,16 +14,31 @@ if( isNaN( day ) )
 
 console.log( `Day ${day}` );
 
-let input = process.argv[ 3 ] || fs.readFileSync( `./data/day${day}.txt` ).toString();
+let runs = 1;
+
+if( process.argv[ 3 ] === 'bench' )
+{
+	delete process.argv[ 3 ];
+	runs = parseInt( process.argv[ 4 ], 10 ) || 1000;
+}
+
+let input = process.argv[ 3 ] || readFileSync( `./data/day${day}.txt` ).toString();
 input = input.replace( /\r/g, '' ).trim();
 
+let result;
 const solution = require( `./js/day${day}.js` );
+const startTime = process.hrtime.bigint();
 
-console.time( 'Time' );
+for( let i = 0; i < runs; i++ )
+{
+	result = solution( input );
+}
 
-const result = solution( input );
-
-console.timeEnd( 'Time' );
+const endTime = process.hrtime.bigint();
 
 console.log( `Part 1: ${result[ 0 ]}` );
 console.log( `Part 2: ${result[ 1 ]}` );
+
+const time = Number( endTime - startTime ) / 1e6 / runs;
+
+console.log( `Time: ${time.toFixed(6)} seconds for ${runs} runs` );
