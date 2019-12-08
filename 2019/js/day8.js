4c5a868a7e8be7f68b688c5e81b12dd4190a1be8
Pavel Djundik <xPaw@users.noreply.github.com>
2019-12-08 19:39:29 +0200

    Make day8 slightly faster

--- 2019/js/day8.js
+++ 2019/js/day8.js
@@ -7,13 +7,19 @@ module.exports = ( input ) =>
 	const w = 25;
 	const h = 6;
 	const stride = w * h;
-	const layers = input.match( new RegExp( '.{1,' + stride + '}', 'g' ) );
-	let lowestCounts = [ Number.MAX_SAFE_INTEGER ];
-	let pixels = new Array( stride ).fill( COLOR_TRANSPARENT );
+	const layersCount = input.length / stride;
+	const layers = new Array( layersCount );
+	const pixels = new Array( stride ).fill( COLOR_TRANSPARENT );
+	let lowestCounts = [ Number.MAX_SAFE_INTEGER, 0, 0 ];
+
+	for( let i = 0, o = 0; i < layersCount; i++, o += stride )
+	{
+		layers[ i ] = input.substr( o, stride );
+	}
 
 	for( const layer of layers )
 	{
-		let counts = new Array( 10 ).fill( 0 );
+		let counts = [ 0, 0, 0 ];
 
 		for( let i = 0; i < stride; i++ )
 		{
@@ -33,7 +39,7 @@ module.exports = ( input ) =>
 	}
 
 	const part1 = lowestCounts[ 1 ] * lowestCounts[ 2 ];
-	let part2 = [];
+	const part2 = [];
 
 	for( let i = 0; i < stride; i++ )
 	{
