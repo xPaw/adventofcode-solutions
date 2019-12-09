9380badf4e61730ae3a7c6d2c3af049914161387
Pavel Djundik <xPaw@users.noreply.github.com>
2019-12-09 11:52:16 +0200

    2019 Day 9

---
+++ 2019/js/day9.js
@@ -0,0 +1,129 @@
+module.exports = ( input ) =>
+{
+	input = input
+		.split( ',' )
+		.map( x => +x );
+
+	let part1 = stateMachine( [ ...input ], [ 1 ] );
+	let part2 = stateMachine( [ ...input ], [ 2 ] );
+
+	return [ part1, part2 ];
+};
+
+function stateMachine( input, ioInput )
+{
+	let output = [];
+	let base = 0;
+	let i = 0;
+	let instruction;
+
+	const getAddress = ( n ) =>
+	{
+		const mode = instruction[ 3 - n ];
+		let offset = i + n;
+
+		switch( mode )
+		{
+			case '0': offset = input[ offset ]; break;
+			//case '1': offset = offset; break;
+			case '2': offset = base + input[ offset ]; break;
+		}
+
+		return offset;
+	};
+	const getValue = ( n ) => input[ getAddress( n ) ] || 0;
+
+halt:
+	while( true )
+	{
+		instruction = input[ i ].toString().padStart( 5, '0' );
+		const opcode = +instruction[ 3 ] * 10 + +instruction[ 4 ];
+
+		//console.log('y', i, 'op:' + opcode, getValue(1), getValue(2), getValue( 3, true ), instruction);
+
+		switch( opcode )
+		{
+			case 1: // add
+			{
+				input[ getAddress( 3) ] = getValue( 1 ) + getValue( 2 );
+				i += 4;
+
+				break;
+			}
+			case 2: // multiply
+			{
+				input[ getAddress( 3 ) ] = getValue( 1 ) * getValue( 2 );
+				i += 4;
+
+				break;
+			}
+			case 3: // input
+			{
+				input[ getAddress( 1 ) ] = ioInput.shift();
+				i += 2;
+
+				break;
+			}
+			case 4: // output
+			{
+				output.push( getValue( 1 ) );
+				i += 2;
+
+				break;
+			}
+			case 5: // jump-if-true
+			{
+				if( getValue( 1 ) !== 0 )
+				{
+					i = getValue( 2 );
+				}
+				else
+				{
+					i += 3;
+				}
+
+				break;
+			}
+			case 6: // jump-if-false
+			{
+				if( getValue( 1 ) === 0 )
+				{
+					i = getValue( 2 );
+				}
+				else
+				{
+					i += 3;
+				}
+
+				break;
+			}
+			case 7: // less than
+			{
+				input[ getAddress( 3 ) ] = getValue( 1 ) < getValue( 2 ) ? 1 : 0;
+				i += 4;
+
+				break;
+			}
+			case 8: // equals
+			{
+				input[ getAddress( 3 ) ] = getValue( 1 ) === getValue( 2 ) ? 1 : 0;
+				i += 4;
+
+				break;
+			}
+			case 9: // modify relative base
+			{
+				base += getValue( 1 );
+				i += 2;
+
+				break;
+			}
+			case 99:
+			{
+				break halt;
+			}
+		}
+	}
+
+	return output.join( ',' );
+}
