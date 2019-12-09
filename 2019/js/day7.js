d468cd765c3d6f7a163abc0a5d54ab173156fb64
Pavel Djundik <xPaw@users.noreply.github.com>
2019-12-09 13:13:54 +0200

    Move intcode to a module, use it in all days

--- 2019/js/day7.js
+++ 2019/js/day7.js
@@ -1,3 +1,5 @@
+const stateMachine = require( '../intcode.js' );
+
 module.exports = ( input ) =>
 {
 	input = input
@@ -15,7 +17,7 @@ module.exports = ( input ) =>
 		for( let i = 0; i < 5; i++ )
 		{
 			const ioInput = [ phase[ i ], signal ];
-			signal = stateMachine( [ ...input ], ioInput );
+			signal = stateMachine( [ ...input ], ioInput ).pop();
 		}
 
 		if( part1 < signal )
@@ -26,97 +28,6 @@ module.exports = ( input ) =>
 
 	return [ part1, part2 ];
 };
-
-function stateMachine( input, ioInput )
-{
-	let lastOutput;
-
-halt:
-	for( let i = 0; i < input.length; i++ )
-	{
-		const instruction = input[ i ].toString().padStart( 5, '0' );
-		const opcode = +instruction[ 3 ] * 10 + +instruction[ 4 ];
-		const getValue = ( n ) => input[ instruction[ 3 - n ] === '1' ? ( i + n ) : input[ i + n ] ];
-
-		switch( opcode )
-		{
-			case 1: // add
-			{
-				input[ input[ i + 3 ] ] = getValue( 1 ) + getValue( 2 );
-				i += 3;
-
-				break;
-			}
-			case 2: // multiply
-			{
-				input[ input[ i + 3 ] ] = getValue( 1 ) * getValue( 2 );
-				i += 3;
-
-				break;
-			}
-			case 3: // input
-			{
-				input[ input[ i + 1 ] ] = ioInput.shift();
-				i += 1;
-
-				break;
-			}
-			case 4: // output
-			{
-				lastOutput = getValue( 1 );
-				i += 1;
-
-				break;
-			}
-			case 5: // jump-if-true
-			{
-				if( getValue( 1 ) !== 0 )
-				{
-					i = getValue( 2 ) - 1;
-				}
-				else
-				{
-					i += 2;
-				}
-
-				break;
-			}
-			case 6: // jump-if-false
-			{
-				if( getValue( 1 ) === 0 )
-				{
-					i = getValue( 2 ) - 1;
-				}
-				else
-				{
-					i += 2;
-				}
-
-				break;
-			}
-			case 7: // less than
-			{
-				input[ input[ i + 3 ] ] = getValue( 1 ) < getValue( 2 ) ? 1 : 0;
-				i += 3;
-
-				break;
-			}
-			case 8: // equals
-			{
-				input[ input[ i + 3 ] ] = getValue( 1 ) === getValue( 2 ) ? 1 : 0;
-				i += 3;
-
-				break;
-			}
-			case 99:
-			{
-				break halt;
-			}
-		}
-	}
-
-	return lastOutput;
-}
 
 function permutator(inputArr)
 {
--- 2019/js/day9.js
+++ 2019/js/day9.js
@@ -1,128 +1,13 @@
+const stateMachine = require( '../intcode.js' );
+
 module.exports = ( input ) =>
 {
 	input = input
 		.split( ',' )
 		.map( x => +x );
 
-	let part1 = stateMachine( [ ...input ], [ 1 ] );
-	let part2 = stateMachine( [ ...input ], [ 2 ] );
+	let part1 = stateMachine( [ ...input ], [ 1 ] ).pop();
+	let part2 = stateMachine( [ ...input ], [ 2 ] ).pop();
 
 	return [ part1, part2 ];
 };
-
-function stateMachine( input, ioInput )
-{
-	let output = [];
-	let base = 0;
-	let i = 0;
-	let instruction;
-	let powers = [ 0, 100, 1000, 10000 ]; // Math.pow is slow
-
-	const getAddress = ( n ) =>
-	{
-		const mode = ~~( instruction / powers[ n ] ) % 10;
-		let offset = i + n;
-
-		switch( mode )
-		{
-			case 0: offset = input[ offset ]; break;
-			//case 1: offset = offset; break;
-			case 2: offset = base + input[ offset ]; break;
-		}
-
-		return offset;
-	};
-	const getValue = ( n ) => input[ getAddress( n ) ] || 0;
-
-halt:
-	while( true )
-	{
-		instruction = input[ i ];
-		const opcode = instruction % 100;
-
-		switch( opcode )
-		{
-			case 1: // add
-			{
-				input[ getAddress( 3 ) ] = getValue( 1 ) + getValue( 2 );
-				i += 4;
-
-				break;
-			}
-			case 2: // multiply
-			{
-				input[ getAddress( 3 ) ] = getValue( 1 ) * getValue( 2 );
-				i += 4;
-
-				break;
-			}
-			case 3: // input
-			{
-				input[ getAddress( 1 ) ] = ioInput.shift();
-				i += 2;
-
-				break;
-			}
-			case 4: // output
-			{
-				output.push( getValue( 1 ) );
-				i += 2;
-
-				break;
-			}
-			case 5: // jump-if-true
-			{
-				if( getValue( 1 ) !== 0 )
-				{
-					i = getValue( 2 );
-				}
-				else
-				{
-					i += 3;
-				}
-
-				break;
-			}
-			case 6: // jump-if-false
-			{
-				if( getValue( 1 ) === 0 )
-				{
-					i = getValue( 2 );
-				}
-				else
-				{
-					i += 3;
-				}
-
-				break;
-			}
-			case 7: // less than
-			{
-				input[ getAddress( 3 ) ] = getValue( 1 ) < getValue( 2 ) ? 1 : 0;
-				i += 4;
-
-				break;
-			}
-			case 8: // equals
-			{
-				input[ getAddress( 3 ) ] = getValue( 1 ) === getValue( 2 ) ? 1 : 0;
-				i += 4;
-
-				break;
-			}
-			case 9: // modify relative base
-			{
-				base += getValue( 1 );
-				i += 2;
-
-				break;
-			}
-			case 99:
-			{
-				break halt;
-			}
-		}
-	}
-
-	return output.join( ',' );
-}
