4b11fc14677956a78ab33ccaf41294cbe3bf3cc6
Pavel Djundik <xPaw@users.noreply.github.com>
2020-12-03 11:03:22 +0200

    2020 Day 3

---
+++ 2020/js/day3.js
@@ -0,0 +1,28 @@
+module.exports = ( input ) =>
+{
+	input = input.split( '\n' ).map( row => row.split( '' ).map( cell => cell === '#' ) );
+
+	let width = input[ 0 ].length;
+
+	const slope = ( right, down ) =>
+	{
+		let trees = 0;
+
+		for( let y = 0; y < input.length; y += down )
+		{
+			const x = ( right * y / down ) % width;
+	
+			if( input[ y ][ x ] )
+			{
+				trees++;
+			}
+		}
+
+		return trees;
+	}
+
+	const part1 = slope( 3, 1 );
+	const part2 = slope( 1, 1 ) * part1 * slope( 5, 1 ) * slope( 7, 1 ) * slope( 1, 2 );
+
+	return [ part1, part2 ];
+};
