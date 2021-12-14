32a941dd5d73e2dd8fcc2baa0974832a3f9acbff
Pavel Djundik <xPaw@users.noreply.github.com>
2021-12-14 11:44:08 +0200

    Hashing tuples is slow

--- 2021/Answers/Solutions/Day14.cs
+++ 2021/Answers/Solutions/Day14.cs
@@ -9,18 +9,19 @@ class Day14 : IAnswer
 {
 	public (string Part1, string Part2) Solve(string input)
 	{
+		const int HASH_SIZE = 10000;
+		var hash = static (int a, int b) => a * HASH_SIZE + b;
 		var lines = input.Split('\n');
 		var polymer = lines[0].ToArray();
-
 		var insertions = lines
 			.Skip(2)
 			.Select(l => l
 				.Split(" -> ")
 				.ToArray()
 			)
-			.ToDictionary(x => (x[0][0], x[0][1]), y => y[1][0]);
+			.ToDictionary(x => hash(x[0][0], x[0][1]), y => y[1][0]);
 
-		var pairs = new Dictionary<(char, char), long>();
+		var pairs = new Dictionary<int, long>();
 		var chars = new Dictionary<char, long>();
 		long part1 = 0;
 
@@ -30,24 +31,24 @@ class Day14 : IAnswer
 
 			if (i < polymer.Length - 1)
 			{
-				var pair = (polymer[i], polymer[i + 1]);
+				var pair = hash(polymer[i], polymer[i + 1]);
 				pairs[pair] = pairs.GetValueOrDefault(pair) + 1;
 			}
 		}
 
 		for (var step = 0; step < 40; step++)
 		{
-			var copy = new Dictionary<(char, char), long>(pairs);
+			var copy = new Dictionary<int, long>(pairs);
 
 			foreach (var pair in copy)
 			{
 				var insert = insertions[pair.Key];
 				pairs[pair.Key] -= pair.Value;
 
-				var newPair = (pair.Key.Item1, insert);
+				var newPair = hash(pair.Key / HASH_SIZE, insert);
 				pairs[newPair] = pairs.GetValueOrDefault(newPair) + pair.Value;
 
-				newPair = (insert, pair.Key.Item2);
+				newPair = hash(insert, pair.Key % HASH_SIZE);
 				pairs[newPair] = pairs.GetValueOrDefault(newPair) + pair.Value;
 				chars[insert] = chars.GetValueOrDefault(insert) + pair.Value;
 			}
