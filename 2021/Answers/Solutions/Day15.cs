966dd34f43fffddc2fba28c41df3c338e8d12b9c
Pavel Djundik <xPaw@users.noreply.github.com>
2021-12-15 13:31:42 +0200

    Optimize

--- 2021/Answers/Solutions/Day15.cs
+++ 2021/Answers/Solutions/Day15.cs
@@ -7,171 +7,86 @@ namespace AdventOfCode2021;
 [Answer(15)]
 class Day15 : IAnswer
 {
+
+	record Tile(int X, int Y, int Cost);
+
 	public (string Part1, string Part2) Solve(string input)
 	{
 		var size = input.IndexOf('\n');
-		var map = input
-			.Split('\n')
-			.Select(l =>
-			{
-				var numbers = new int[l.Length];
+		var map = new int[size * size];
+		var offset = 0;
 
-				for (var i = 0; i < numbers.Length; i++)
-				{
-					numbers[i] = l[i] - '0';
-				}
-
-				return numbers;
-			})
-			.ToArray();
-
-
-		var part1 = Solve(map, size);
-
-		var bigMap = new int[size * 5][];
-
-		for (var y = 0; y < size * 5; y++)
+		for (var i = 0; i < input.Length; i++)
 		{
-			bigMap[y] = new int[size * 5];
-		}
-
-		for (var y = 0; y < size; y++)
-		{
-			for (var x = 0; x < size; x++)
+			if (input[i] == '\n')
 			{
-				bigMap[y][x] = map[y][x];
-			}
-		}
-
-		for (var i = 1; i < 5; i++)
-		{
-			for (var y = 0; y < size; y++)
-			{
-				for (var x = i * size; x < size * 5; x++)
-				{
-					var n = bigMap[y][x - size] + 1;
-
-					if (n > 9)
-					{
-						n = 1;
-					}
-
-					bigMap[y][x] = n;
-				}
+				continue;
 			}
 
-			for (var y = i * size; y < size * 5; y++)
-			{
-				for (var x = 0; x < size * 5; x++)
-				{
-					var n = bigMap[y - size][x] + 1;
-
-					if (n > 9)
-					{
-						n = 1;
-					}
-
-					bigMap[y][x] = n;
-				}
-			}
+			map[offset++] = input[i] - '0';
 		}
 
-		var part2 = Solve(bigMap, size * 5);
+		var part1 = Solve(map, size, size - 1);
+		var part2 = Solve(map, size, size * 5 - 1);
 
 		return (part1.ToString(), part2.ToString());
 	}
 
-	private int Solve(int[][] map, int size)
+	private int Solve(int[] map, int size, int finish)
 	{
-		var answer = 0;
-		var start = new Tile
-		{
-			Y = 0,
-			X = 0,
-		};
+		var start = new Tile(0, 0, 0);
 		var visitedTiles = new HashSet<int>();
-		var activeTiles = new Dictionary<int, Tile>
+		var activeTiles = new PriorityQueue<Tile, int>();
+		activeTiles.Enqueue(start, 0);
+
+		var Enqueue = (Tile walkableTile) =>
 		{
-			{ 0, start },
+			var hash = walkableTile.Y * 10000 + walkableTile.X;
+
+			if (visitedTiles.Contains(hash))
+			{
+				return;
+			}
+
+			visitedTiles.Add(hash);
+
+			var y = Math.DivRem(walkableTile.Y, size);
+			var x = Math.DivRem(walkableTile.X, size);
+
+			var cost = (map[y.Remainder * size + x.Remainder] + y.Quotient + x.Quotient - 1) % 9 + 1;
+			cost += walkableTile.Cost;
+
+			activeTiles.Enqueue(new Tile(walkableTile.X, walkableTile.Y, cost), cost);
 		};
 
 		while (true)
 		{
-			var checkTile = start;
-			var cost = int.MaxValue;
+			var checkTile = activeTiles.Dequeue();
 
-			foreach (var tile in activeTiles.Values)
+			if (checkTile.X == finish && checkTile.Y == finish)
 			{
-				if (cost > tile.Cost)
-				{
-					checkTile = tile;
-					cost = tile.Cost;
-				}
+				return checkTile.Cost;
 			}
-
-			if (checkTile.X == size - 1 && checkTile.Y == size - 1)
-			{
-				while (checkTile.Y > 0 || checkTile.X > 0)
-				{
-					answer += map[checkTile.Y][checkTile.X];
-					checkTile = checkTile.Parent!;
-				}
-
-				break;
-			}
-
-			var hash = checkTile.Y * size + checkTile.X;
-			visitedTiles.Add(hash);
-			activeTiles.Remove(hash);
-
-			var walkableTiles = new List<Tile>();
 
 			if (checkTile.Y > 0)
 			{
-				walkableTiles.Add(new Tile { X = checkTile.X, Y = checkTile.Y - 1, Parent = checkTile, Cost = checkTile.Cost });
+				Enqueue(new Tile(checkTile.X, checkTile.Y - 1, checkTile.Cost));
 			}
 
-			if (checkTile.Y < size - 1)
+			if (checkTile.Y < finish)
 			{
-				walkableTiles.Add(new Tile { X = checkTile.X, Y = checkTile.Y + 1, Parent = checkTile, Cost = checkTile.Cost });
+				Enqueue(new Tile(checkTile.X, checkTile.Y + 1, checkTile.Cost));
 			}
 
 			if (checkTile.X > 0)
 			{
-				walkableTiles.Add(new Tile { X = checkTile.X - 1, Y = checkTile.Y, Parent = checkTile, Cost = checkTile.Cost });
+				Enqueue(new Tile(checkTile.X - 1, checkTile.Y, checkTile.Cost));
 			}
 
-			if (checkTile.X < size - 1)
+			if (checkTile.X < finish)
 			{
-				walkableTiles.Add(new Tile { X = checkTile.X + 1, Y = checkTile.Y, Parent = checkTile, Cost = checkTile.Cost });
-			}
-
-			foreach (var walkableTile in walkableTiles)
-			{
-				hash = walkableTile.Y * size + walkableTile.X;
-
-				if (visitedTiles.Contains(hash))
-				{
-					continue;
-				}
-
-				walkableTile.Cost += map[walkableTile.Y][walkableTile.X];
-
-				if (!activeTiles.ContainsKey(hash))
-				{
-					activeTiles[hash] = walkableTile;
-				}
+				Enqueue(new Tile(checkTile.X + 1, checkTile.Y, checkTile.Cost));
 			}
 		}
-
-		return answer;
-	}
-
-	class Tile
-	{
-		public int X { get; set; }
-		public int Y { get; set; }
-		public int Cost { get; set; }
-		public Tile? Parent { get; set; }
 	}
 }
