d9ca51927ed90f9929de2447b2e27c8c9497644a
Pavel Djundik <xPaw@users.noreply.github.com>
2021-12-16 23:22:31 +0200

    benchmark all days at once

--- 2021/Answers/Program.cs
+++ 2021/Answers/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Globalization;
 using System.Text;
+using System.Threading.Tasks;
 using AdventOfCode2021;
 
 Console.OutputEncoding = Encoding.UTF8;
@@ -12,6 +13,12 @@ var runs = 1;
 
 if (args.Length > 0)
 {
+	if (args[0] == "combined")
+	{
+		await BenchmarkAllDays();
+		return 0;
+	}
+
 	if (args[0] != "today" && !int.TryParse(args[0], out day))
 	{
 		Console.Error.WriteLine("Usage: [day [runs]]");
@@ -119,3 +126,53 @@ else
 }
 
 return 0;
+
+static async Task BenchmarkAllDays()
+{
+	double combined = 0d;
+
+	Console.WriteLine($"{"Day",-10} {"Runs",-10} Time");
+
+	for (var day = 1; day < 17; day++)
+	{
+		var runs = day == 12 || day == 15 ? 100 : 5000;
+
+		Console.Write($"{day,-10} {runs,-10} ");
+
+		var data = await Solver.LoadData(day);
+		var type = Solver.GetSolutionType(day);
+		var stopWatch = new Stopwatch();
+		double total = 0d;
+
+		for (var i = 1; i < runs; i++)
+		{
+			stopWatch.Restart();
+			Solver.Solve(type, data);
+			stopWatch.Stop();
+
+			var elapsed = stopWatch.Elapsed.TotalMilliseconds;
+			total += elapsed;
+		}
+
+		var time = total / runs;
+		combined += time;
+
+		if (time >= 1.0d)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+		}
+		else if (time <= 0.1d)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+		}
+
+		Console.WriteLine($"{time:N6}");
+		Console.ResetColor();
+	}
+
+	Console.Write($"{"Total",-10} {"-",-10} ");
+	Console.ForegroundColor = ConsoleColor.Blue;
+	Console.Write($"{combined:N6}");
+	Console.ResetColor();
+	Console.WriteLine("ms");
+}
