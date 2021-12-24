4c497645acf864518466a0ef1546d2497a1bf8d7
Pavel Djundik <xPaw@users.noreply.github.com>
2021-12-24 18:30:29 +0200

    Calculate answers without bruteforcing

--- 2021/Answers/Solutions/Day24.cs
+++ 2021/Answers/Solutions/Day24.cs
@@ -3,115 +3,70 @@ using System.Collections.Generic;
 
 namespace AdventOfCode2021;
 
-[Answer(24, slow: true)]
+[Answer(24)]
 class Day24 : IAnswer
 {
 	readonly int[] a = new int[14];
 	readonly int[] b = new int[14];
 	readonly int[] c = new int[14];
-	readonly int[] stackLookup = new int[14];
 
 	public (string Part1, string Part2) Solve(string input)
 	{
 		var lines = input.Split('\n');
-		var stack = new Stack<int>(7);
 
 		for (var i = 0; i < 14; i++)
 		{
 			a[i] = ParseNumber(lines[4 + 18 * i]);
 			b[i] = ParseNumber(lines[5 + 18 * i]);
 			c[i] = ParseNumber(lines[15 + 18 * i]);
-
-			if (a[i] == 1)
-			{
-				stack.Push(i);
-			}
-			else
-			{
-				stackLookup[i] = stack.Pop();
-			}
 		}
 
-		var part1 = Solve(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 });
-		var part2 = Solve(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+		var part1 = Solve(true);
+		var part2 = Solve(false);
 
 		return (part1.ToString(), part2.ToString());
 	}
 
 	int ParseNumber(string line) => int.Parse(line[line.LastIndexOf(' ')..]);
 
-	long Solve(int[] range)
+	long Solve(bool max)
 	{
 		var number = new int[14];
+		var stack = new Stack<int>(7);
 
-		foreach (var n1 in range)
+		for (var i = 0; i < 14; i++)
 		{
-			foreach (var n2 in range)
+			if (a[i] == 1)
 			{
-				foreach (var n3 in range)
+				number[i] = max ? 9 : 1;
+				stack.Push(i);
+			}
+			else
+			{
+				var y = stack.Pop();
+				var digit = number[y] + c[y] + b[i];
+				number[i] = digit;
+
+				if (digit > 9)
 				{
-					foreach (var n4 in range)
-					{
-						foreach (var n5 in range)
-						{
-							foreach (var n6 in range)
-							{
-								foreach (var n7 in range)
-								{
-									var digit = 0;
-									var bad = false;
-
-									for (var i = 0; i < 14; i++)
-									{
-										if (a[i] == 1)
-										{
-											number[i] = digit switch
-											{
-												0 => n1,
-												1 => n2,
-												2 => n3,
-												3 => n4,
-												4 => n5,
-												5 => n6,
-												6 => n7,
-												_ => throw new NotImplementedException(),
-											};
-											digit++;
-										}
-										else
-										{
-											var y = stackLookup[i];
-											number[i] = number[y] + c[y] + b[i];
-
-											if (number[i] < 1 || number[i] > 9)
-											{
-												bad = true;
-												break;
-											}
-										}
-									}
-
-									if (bad)
-									{
-										continue;
-									}
-
-									long n = 0;
-
-									for (var i = 0; i < 14; i++)
-									{
-										n = 10 * n + number[i];
-									}
-
-									return n;
-								}
-							}
-						}
-					}
+					number[y] -= digit - 9;
+					number[i] = 9;
+				}
+				else if (digit < 1)
+				{
+					number[y] += 1 - digit;
+					number[i] = 1;
 				}
 			}
 		}
 
-		return 0;
+		long n = 0;
+
+		for (var i = 0; i < 14; i++)
+		{
+			n = 10 * n + number[i];
+		}
+
+		return n;
 	}
 }
