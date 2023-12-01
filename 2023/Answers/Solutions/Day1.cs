4f1ca6ca27f55c21e1d9b3d5c3730c9dee0e278d
Pavel Djundik <xPaw@users.noreply.github.com>
2023-12-01 15:46:03 +0200

    Faster

--- 2023/Answers/Solutions/Day1.cs
+++ 2023/Answers/Solutions/Day1.cs
@@ -1,25 +1,11 @@
 using System;
+using System.Text;
 
 namespace AdventOfCode;
 
 [Answer(1)]
 public class Day1() : IAnswer
 {
-	private readonly static string[] Words =
-	[
-		"one",
-		"two",
-		"three",
-		"four",
-		"five",
-		"six",
-		"seven",
-		"eight",
-		"nine",
-	];
-
-	static bool IsDigitLetter(char c) => c is 'e' or 'f' or 'n' or 'o' or 's' or 't';
-
 	public Solution Solve(string input)
 	{
 		var part1 = 0;
@@ -27,64 +13,33 @@ public class Day1() : IAnswer
 
 		foreach (var line in input.AsSpan().EnumerateLines())
 		{
-			var first = 0;
-			var last = 0;
-			var first2 = 0;
-			var last2 = 0;
+			part1 += Score(line);
+		}
 
-			for (var i = 0; i < line.Length; i++)
-			{
-				var c = line[i];
+		var sb = new StringBuilder(input)
+			.Replace("one", "o1e")
+			.Replace("two", "t2o")
+			.Replace("three", "t3e")
+			.Replace("four", "f4r")
+			.Replace("five", "f5e")
+			.Replace("six", "s6x")
+			.Replace("seven", "s7n")
+			.Replace("eight", "e8t")
+			.Replace("nine", "n9e");
 
-				if (char.IsAsciiDigit(c))
-				{
-					var num = c - '0';
-
-					if (first == 0)
-					{
-						first = num;
-					}
-
-					if (first2 == 0)
-					{
-						first2 = num;
-					}
-
-					last = num;
-					last2 = num;
-
-					continue;
-				}
-
-				if (i <= line.Length - 3 && IsDigitLetter(c))
-				{
-					for (var counter = 0; counter < Words.Length; counter++)
-					{
-						var test = Words[counter];
-						var end = i + test.Length;
-
-						if (end <= line.Length && line[i..end].Equals(test, StringComparison.Ordinal))
-						{
-							i += test.Length - 2; // skip forward but allow matching overlapping word
-							counter += 1;
-
-							if (first2 == 0)
-							{
-								first2 = counter;
-							}
-
-							last2 = counter;
-
-							break;
-						}
-					}
-				}
-			}
-
-			part1 += first * 10 + last;
-			part2 += first2 * 10 + last2;
+		foreach (var line in sb.ToString().AsSpan().EnumerateLines())
+		{
+			part2 += Score(line);
 		}
 
 		return new(part1.ToString(), part2.ToString());
+	}
+
+	private static int Score(ReadOnlySpan<char> line)
+	{
+		var first = line[line.IndexOfAnyInRange('0', '9')] - '0';
+		var last = line[line.LastIndexOfAnyInRange('0', '9')] - '0';
+
+		return first * 10 + last;
 	}
 }
