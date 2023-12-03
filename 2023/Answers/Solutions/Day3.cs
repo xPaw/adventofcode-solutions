b00524631bbf794f8e94d397b4813a7543dd7ce2
Pavel Djundik <xPaw@users.noreply.github.com>
2023-12-03 12:48:36 +0200

    Flip ifs

--- 2023/Answers/Solutions/Day3.cs
+++ 2023/Answers/Solutions/Day3.cs
@@ -19,51 +19,48 @@ public class Day3 : IAnswer
 		{
 			var c = input[i];
 
-			if (c == '\n' || c == '.')
+			if (c == '\n' || c == '.' || char.IsAsciiDigit(c))
 			{
 				continue;
 			}
 
-			if (!char.IsAsciiDigit(c))
+			var (y, x) = Math.DivRem(i, size);
+			var isGear = c == '*';
+			var leftNumber = 0;
+
+			for (int y2 = y - 1; y2 <= y + 1; y2++)
 			{
-				var (y, x) = Math.DivRem(i, size);
-				var isGear = c == '*';
-				var leftNumber = 0;
-
-				for (int y2 = y - 1; y2 <= y + 1; y2++)
+				for (int x2 = x - 1; x2 <= x + 1; x2++)
 				{
-					for (int x2 = x - 1; x2 <= x + 1; x2++)
+					if (!char.IsAsciiDigit(GetAtPosition(x2, y2)))
 					{
-						var c2 = GetAtPosition(x2, y2);
+						continue;
+					}
 
-						if (char.IsAsciiDigit(c2))
+					while (--x2 >= 0 && char.IsAsciiDigit(GetAtPosition(x2, y2)))
+					{
+						//
+					}
+
+					var number = 0;
+
+					do
+					{
+						number = number * 10 + (GetAtPosition(++x2, y2) - '0');
+					}
+					while (char.IsAsciiDigit(GetAtPosition(x2 + 1, y2)));
+
+					part1 += number;
+
+					if (isGear)
+					{
+						if (leftNumber > 0)
 						{
-							while (--x2 >= 0 && char.IsAsciiDigit(GetAtPosition(x2, y2)))
-							{
-								//
-							}
-
-							var number = 0;
-
-							do
-							{
-								number = number * 10 + (GetAtPosition(++x2, y2) - '0');
-							}
-							while (char.IsAsciiDigit(GetAtPosition(x2 + 1, y2)));
-
-							part1 += number;
-
-							if (isGear)
-							{
-								if (leftNumber > 0)
-								{
-									part2 += leftNumber * number;
-								}
-								else
-								{
-									leftNumber = number;
-								}
-							}
+							part2 += leftNumber * number;
+						}
+						else
+						{
+							leftNumber = number;
 						}
 					}
 				}
