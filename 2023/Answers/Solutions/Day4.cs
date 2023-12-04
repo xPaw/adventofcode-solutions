c5abee82987b170a70353cfa6959bcfa713964df
Pavel Djundik <xPaw@users.noreply.github.com>
2023-12-04 12:26:21 +0200

    Go faster

--- 2023/Answers/Solutions/Day4.cs
+++ 2023/Answers/Solutions/Day4.cs
@@ -1,66 +1,62 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace AdventOfCode;
 
 [Answer(4)]
 public class Day4 : IAnswer
 {
-	public Solution Solve(string input)
+	public Solution Solve(string inputStr)
 	{
+		var input = inputStr.AsSpan();
 		var part1 = 0;
 		var part2 = 0;
-		var cardId = 0;
-		var offset = input.IndexOf(':') + 1;
-		var winningNumbers = new HashSet<int>(10);
-		var cards = Enumerable.Repeat(1, 200).ToArray();
 
-		foreach (var line in input.AsSpan().EnumerateLines())
+		var offset = input.IndexOf(':') + 2;
+		var offsetSplit = input.IndexOf('|') + 2;
+		var offsetLine = input.IndexOf('\n') + 1;
+
+		var winnersCount = (offsetSplit - offset) / 3;
+		var guessesCount = (offsetLine - (offsetSplit - 2)) / 3;
+		var winningNumbers = new int[winnersCount].AsSpan();
+
+		var cardCount = input.Length / offsetLine;
+		var cards = new int[cardCount + winnersCount];
+		cards.AsSpan().Fill(1);
+
+		static int GetNumber(ReadOnlySpan<char> line)
 		{
-			var i = 0;
-			var i2 = offset;
-			var parsingWinningNumbers = true;
+			var a = line[0] - '0';
+			var b = line[1] - '0';
+
+			return a < 0 ? b : a * 10 + b;
+		}
+
+		for (int cardId = 0; cardId <= cardCount; cardId++)
+		{
 			var winners = 0;
+			var off = offsetLine * cardId + offset;
 
-			do
+			for (var i = 0; i < winnersCount; i++)
 			{
-				i += i2;
+				winningNumbers[i] = GetNumber(input[off..]);
+				off += 3;
+			}
 
-				var number = 0;
+			off += 2;
 
-				do
-				{
-					i++;
-				}
-				while (line[i] == ' ');
+			for (var i = 0; i < guessesCount; i++)
+			{
+				var number = GetNumber(input[off..]);
+				off += 3;
 
-				if (line[i] == '|')
-				{
-					parsingWinningNumbers = false;
-					i += line[(i + 1)..].IndexOfAnyExcept(' ') + 1;
-				}
-
-				do
-				{
-					number = number * 10 + (line[i] - '0');
-				}
-				while (++i < line.Length && line[i] != ' ');
-
-				if (parsingWinningNumbers)
-				{
-					winningNumbers.Add(number);
-				}
-				else if (winningNumbers.Contains(number))
+				if (winningNumbers.Contains(number))
 				{
 					winners++;
 				}
 			}
-			while ((i2 = line[i..].IndexOf(' ')) != -1);
-
-			winningNumbers.Clear();
 
 			var previousCards = cards[cardId];
+			part2 += previousCards;
 
 			if (winners > 0)
 			{
@@ -71,10 +67,6 @@ public class Day4 : IAnswer
 					cards[c] += previousCards;
 				}
 			}
-
-			part2 += previousCards;
-
-			cardId++;
 		}
 
 		return new(part1.ToString(), part2.ToString());
