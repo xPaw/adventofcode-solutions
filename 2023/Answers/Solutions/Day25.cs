9a2cf9f855b2d15d8e572d774f39b2734a1c3ff7
Pavel Djundik <xPaw@users.noreply.github.com>
2023-12-26 23:00:33 +0200

    Non random day 25

--- 2023/Answers/Solutions/Day25.cs
+++ 2023/Answers/Solutions/Day25.cs
@@ -7,12 +7,9 @@ namespace AdventOfCode;
 [Answer(25)]
 public class Day25 : IAnswer
 {
-	record struct Edge(string Left, string Right);
-
 	public Solution Solve(string input)
 	{
-		var nodes = new HashSet<string>();
-		var edges = new HashSet<Edge>();
+		var graph = new Dictionary<string, HashSet<string>>();
 
 		foreach (var line in input.AsSpan().EnumerateLines())
 		{
@@ -20,60 +17,36 @@ public class Day25 : IAnswer
 			var name = line[..colon].ToString();
 			var destinations = line[(colon + 2)..].ToString().Split(' ').ToHashSet();
 
-			nodes.Add(name);
+			if (!graph.TryGetValue(name, out var list1))
+			{
+				list1 = [];
+				graph[name] = list1;
+			}
 
 			foreach (var node in destinations)
 			{
-				nodes.Add(node);
+				list1.Add(node);
 
-				if (!edges.Contains(new(name, node)) && !edges.Contains(new(node, name)))
+				if (!graph.TryGetValue(node, out var list2))
 				{
-					edges.Add(new(name, node));
+					list2 = [];
+					graph[node] = list2;
 				}
+
+				list2.Add(name);
 			}
 		}
 
-		List<HashSet<string>> groups = [];
-		var wires = 0;
+		var edges = new HashSet<string>(graph.Keys);
 
-		while (wires != 3)
+		int Count(string node) => graph[node].Count(n => !edges.Contains(n));
+
+		while (edges.Sum(Count) != 3)
 		{
-			wires = 0;
-			groups = nodes.Select(n => new HashSet<string>([n])).ToList();
-
-			var randomEdges = edges.ToArray();
-			Random.Shared.Shuffle(randomEdges);
-
-			for (var i = 0; i < edges.Count && groups.Count > 2; i++)
-			{
-				var (left, right) = randomEdges[i];
-				var group1 = groups.First(s => s.Contains(left));
-				var group2 = groups.First(s => s.Contains(right));
-
-				if (group1 != group2)
-				{
-					foreach (var node in group2)
-					{
-						group1.Add(node);
-					}
-
-					groups.Remove(group2);
-				}
-			}
-
-			foreach (var (left, right) in edges)
-			{
-				var group1 = groups.First(s => s.Contains(left));
-				var group2 = groups.First(s => s.Contains(right));
-
-				if (group1 != group2)
-				{
-					wires++;
-				}
-			}
+			edges.Remove(edges.MaxBy(Count)!);
 		}
 
-		var part1 = groups[0].Count * groups[1].Count;
+		var part1 = edges.Count * graph.Keys.Except(edges).Count();
 		var part2 = 0;
 
 		return new(part1.ToString(), part2.ToString());
