cba65bacace3f175f97ca292739886792ec2f0a3
Pavel Djundik <xPaw@users.noreply.github.com>
2024-12-21 13:29:02 +0200

    Part 2

--- 2024/Answers/Solutions/Day21.cs
+++ 2024/Answers/Solutions/Day21.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace AdventOfCode;
 
@@ -9,97 +8,130 @@ public class Day21 : IAnswer
 {
 	public Solution Solve(string input)
 	{
-		var part1 = 0;
-		var part2 = 0;
 		var pad = new ReadOnlyGrid("789\n456\n123\n 0A", ' ');
 		var dir = new ReadOnlyGrid(" ^A\n<v>", ' ');
+		var cache = new Dictionary<int, long>(1024);
+		var part1 = 0L;
+		var part2 = 0L;
 
 		foreach (var line in input.AsSpan().EnumerateLines())
 		{
-			var pressed = Move(pad, line.ToArray());
-			var min = int.MaxValue;
+			var len = 0L;
+			var current = 'A';
 
-			foreach (var t1 in pressed)
+			foreach (var target in line)
 			{
-				var t2 = Move(dir, t1);
-
-				foreach (var t3 in t2)
-				{
-					var t4 = Move(dir, t3);
-
-					foreach (var t5 in t4)
-					{
-						if (t5.Length < min)
-						{
-							min = t5.Length;
-						}
-					}
-				}
+				len += Simulate(cache, pad, dir, current, target, 0, 3);
+				current = target;
 			}
 
 			var numericValue = int.Parse(line[..(line.Length - 1)]);
-			part1 += min * numericValue;
+			part1 += numericValue * len;
+		}
+
+		cache.Clear();
+
+		foreach (var line in input.AsSpan().EnumerateLines())
+		{
+			var len = 0L;
+			var current = 'A';
+
+			foreach (var target in line)
+			{
+				len += Simulate(cache, pad, dir, current, target, 0, 26);
+				current = target;
+			}
+
+			var numericValue = int.Parse(line[..(line.Length - 1)]);
+			part2 += numericValue * len;
 		}
 
 		return new(part1.ToString(), part2.ToString());
 	}
 
-	List<string> Move(ReadOnlyGrid grid, IEnumerable<char> line)
+	long Simulate(Dictionary<int, long> cache, ReadOnlyGrid pad, ReadOnlyGrid dir, char from, char to, int depth, int max)
 	{
-		var queue = new Queue<(Coord c, string pressed)>();
-		var paths = new List<string>();
-
-		queue.Enqueue((grid.IndexOf('A'), ""));
-
-		var i = 0;
-
-		foreach (var c in line)
+		if (depth == max)
 		{
-			var end = ++i == line.Count();
-			var target = grid.IndexOf(c);
-			var queue2 = new Queue<(Coord c, string pressed)>();
+			return 1;
+		}
 
-			while (queue.TryDequeue(out var prev))
+		var hash = (from - '0') * 100000 + (to - '0') * 1000 + depth;
+
+		if (cache.TryGetValue(hash, out var cached))
+		{
+			return cached;
+		}
+
+		var min = long.MaxValue;
+		var grid = depth == 0 ? pad : dir;
+		var paths = Move(grid, grid.IndexOf(from), grid.IndexOf(to));
+
+		foreach (var path in paths)
+		{
+			var len = 0L;
+			var current = 'A';
+
+			foreach (var next in path)
 			{
-				var pos = prev.c;
-
-				if (target == pos)
-				{
-					if (end)
-					{
-						paths.Add(prev.pressed + 'A');
-					}
-					else
-					{
-						queue2.Enqueue((pos, prev.pressed + 'A'));
-					}
-				}
-
-				if (target.X < pos.X && grid[pos.Y, pos.X - 1] != ' ')
-				{
-					var newPos = pos - (1, 0);
-					queue.Enqueue((newPos, prev.pressed + '<'));
-				}
-				else if (target.X > pos.X && grid[pos.Y, pos.X + 1] != ' ')
-				{
-					var newPos = pos + (1, 0);
-					queue.Enqueue((newPos, prev.pressed + '>'));
-				}
-
-				if (target.Y < pos.Y && grid[pos.Y - 1, pos.X] != ' ')
-				{
-					var newPos = pos - (0, 1);
-					queue.Enqueue((newPos, prev.pressed + '^'));
-				}
-				else if (target.Y > pos.Y && grid[pos.Y + 1, pos.X] != ' ')
-				{
-					var newPos = pos + (0, 1);
-					queue.Enqueue((newPos, prev.pressed + 'v'));
-				}
-
+				len += Simulate(cache, pad, dir, current, next, depth + 1, max);
+				current = next;
 			}
 
-			queue = queue2;
+			if (min > len)
+			{
+				min = len;
+			}
+		}
+
+		cache[hash] = min;
+
+		return min;
+	}
+
+	List<string> Move(ReadOnlyGrid grid, Coord pos, Coord target)
+	{
+		var paths = new List<string>();
+
+		if (pos == target)
+		{
+			paths.Add("A");
+			return paths;
+		}
+
+		if (grid[pos] == ' ')
+		{
+			return paths;
+		}
+
+		if (target.X < pos.X)
+		{
+			foreach (var p in Move(grid, pos - (1, 0), target))
+			{
+				paths.Add("<" + p);
+			}
+		}
+		else if (target.X > pos.X)
+		{
+			foreach (var p in Move(grid, pos + (1, 0), target))
+			{
+				paths.Add(">" + p);
+			}
+		}
+
+		if (target.Y < pos.Y)
+		{
+			foreach (var p in Move(grid, pos - (0, 1), target))
+			{
+				paths.Add("^" + p);
+			}
+		}
+		else if (target.Y > pos.Y)
+		{
+			foreach (var p in Move(grid, pos + (0, 1), target))
+			{
+				paths.Add("v" + p);
+			}
 		}
 
 		return paths;
