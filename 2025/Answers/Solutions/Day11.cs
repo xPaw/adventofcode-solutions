92d52ed4645ef75d93022b2528d38bbcf1975bae
Pavel Djundik <xPaw@users.noreply.github.com>
2025-12-11 11:41:35 +0200

    2025 Day 11

---
+++ 2025/Answers/Solutions/Day11.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode;
+
+[Answer(11)]
+public class Day11 : IAnswer
+{
+	public Solution Solve(string input)
+	{
+		var graph = new Dictionary<string, string[]>(1024);
+		var cache = new Dictionary<int, long>(2048);
+
+		foreach (var line in input.AsSpan().EnumerateLines())
+		{
+			var colon = line.IndexOf(':');
+			var name = line[..colon].ToString();
+			var destinations = line[(colon + 2)..].ToString().Split(' ');
+			graph[name] = destinations;
+		}
+
+		long CountPaths(string start, string end)
+		{
+			if (start == end)
+			{
+				return 1;
+			}
+
+			var key = HashCode.Combine(start, end);
+
+			if (cache.TryGetValue(key, out var count))
+			{
+				return count;
+			}
+
+			if (!graph.TryGetValue(start, out var neighbors))
+			{
+				return 0;
+			}
+
+			foreach (var next in neighbors)
+			{
+				count += CountPaths(next, end);
+			}
+
+			cache[key] = count;
+
+			return count;
+		}
+
+		var part1 = CountPaths("you", "out");
+		cache.Clear();
+		var part2 = CountPaths("svr", "fft") * CountPaths("fft", "dac") * CountPaths("dac", "out");
+
+		return new(part1.ToString(), part2.ToString());
+	}
+}
